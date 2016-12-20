a61a7921c1d3d934cc16f85ea3a60409f17981e0
VectorLu <vectorpinglu@outlook.com>
2016-12-20 09:34:05 +0800

    array init

---
+++ arrayInit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // 整形数组的初始化
+    int arrayInt[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    // 字符型数组的初始化
+    // 1. 无尺寸数组，C 自动完成计算数组大小的工作
+    char arrayChar1[] = "How are you?";
+    char arrayChar2[13] = "How are you?";
+    char 
+    return 0;
+}
