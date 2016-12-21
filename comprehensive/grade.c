62bc585585c5aa20a1dfc47f20d7fbc61b3f8299
VectorLu <vectorpinglu@outlook.com>
2016-12-21 09:51:08 +0800

    注意类型转换

--- comprehensive/grade.c
+++ comprehensive/grade.c
@@ -1,3 +1,6 @@
+/*
+
+*/
 #include <stdio.h>
 
 #define COR_NUM 5
@@ -9,15 +12,15 @@ float a_stu[COR_NUM], a_cor[STU_NUM];
 void input_stu();
 void avr_stu();
 void avr_cor();
-float highest(int *r, int *c);
+float highest(int *f_stu_no, int *f_cor_no);
 float s_diff();
 
 int main()
 {
-    int i, j, r, c;
+    int i, j, stu_no, cor_no;
     float h;
-    r = 0;
-    c = 1;
+    stu_no = 0;
+    cor_no = 1;
     input_stu();
     avr_stu();
     avr_cor();
@@ -27,8 +30,97 @@ int main()
         printf("\n NO%2d", i+1);
         for (j = 0; j < COR_NUM; j++)
         {
-            printf("%8.2f")
+            printf("%8.2f", score[i][j]);
+        }
+        printf("%8.2f", a_stu[i]);
+    }
+    printf("\n 课平均");
+    for (j = 0; j < COR_NUM; j++)
+    {
+        printf("%8.2f", a_cor[j]);
+    }
+    h = highest(&stu_no, &cor_no);
+    printf("\n\n最高分 %8.2f 是 %d 号学生的第 %d 门课\n", h, stu_no, cor_no);
+    printf("方差 %8.2f\n", s_diff());
+
+    return 0;
+}
+
+void input_stu()
+{
+    int i, j;
+    for (i = 0; i < STU_NUM; i++)
+    {
+        printf("请输入学生%2d 的 5 个成绩：\n", i+1);
+        for (j = 0; j < COR_NUM; j++)
+        {
+            scanf("%f", &score[i][j]);
         }
     }
-    return 0;
+}
+
+void avr_stu()
+{
+    int i, j;
+    float s;
+    for (i = 0; i < STU_NUM; i++)
+    {
+        s = 0;
+        for (j = 0; j < COR_NUM; j++)
+        {
+            s += score[i][j];
+        }
+        // 注意这里有隐式类型转换
+        a_stu[i] = s/COR_NUM;
+    }
+}
+
+void avr_cor()
+{
+    int i, j;
+    float s;
+    for (j = 0; j < COR_NUM; j++)
+    {
+        s = 0;
+        for (i = 0; i < STU_NUM; i++)
+        {
+            s += score[i][j];
+        }
+        // 这里使用了强制类型转换
+        a_cor[j] = s/(float)STU_NUM;
+    }
+}
+
+float highest(int *f_stu_no, int *f_cor_no)
+{
+    float high;
+    int i, j;
+    high = score[0][0];
+    for(i = 0; i < STU_NUM; i++)
+    {
+        for (j = 0; j < COR_NUM; j++)
+        {
+            if (score[i][j] > high)
+            {
+                high = score[i][j];
+                *f_stu_no = i+1;
+                *f_cor_no = j+1;
+            }
+        }
+    }
+    return high;
+}
+
+float s_diff()
+{
+    int i;
+    float sumx, sumxn;
+    sumx = 0.0f;
+    sumxn = 0.0f;
+    for (i = 0; i < STU_NUM; i++)
+    {
+        sumx += a_stu[i]*a_stu[i];
+        sumxn += a_stu[i];
+    }
+    return (sumx/STU_NUM - (sumxn/STU_NUM)*(sumxn/STU_NUM));
 }
